0dc32640b50315d435e798575e29d0567250e256
jagadeesh <jagadeesh.m@innoppl.com>
2022-05-20 22:38:40 +0530

    second commit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongo = require('./shared');
 const getRouter = require('./Router/hallbooking')
-
-
-dotenv.config();
+const cors = require("cors");
 const app = express();
 
+dotenv.config();
+app.use(cors());
 app.use(express.json());
 mongo.connect();
 
